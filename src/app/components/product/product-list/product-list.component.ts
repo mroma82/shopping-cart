a57c12a06a388e4eb084dbb54ec706ae1fc53b73
Michael Roma <mike@romacode.com>
2022-08-15 06:25:26 -0400

    cleanup, add to cart from product list

--- src/app/components/product/product-list/product-list.component.ts
+++ src/app/components/product/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ProductModel } from '../../../models/product-model';
+import { CartContextService } from '../../../services/context/cart-context.service';
 
 @Component({
   selector: 'app-product-list',
@@ -10,9 +11,16 @@ export class ProductListComponent implements OnInit {
   @Input() title?: string;
   @Input() products?: ProductModel[];
 
-  constructor() { }
+  // new
+  constructor(
+    private cartContext: CartContextService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  // add to cart
+  addToCart(product: ProductModel) {
+    this.cartContext.addToCart(product, 1);
+  }
 }
