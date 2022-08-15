255b9289ea106615785ea10c8df7070eef0304d1
Michael Roma <mike@romacode.com>
2022-08-15 06:34:06 -0400

    catalog page

--- src/app/pages/catalog-page/catalog-page.component.ts
+++ src/app/pages/catalog-page/catalog-page.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { CatalogPageContextService } from '../../services/context/catalog-page-context.service';
 
 @Component({
   selector: 'app-catalog-page',
   templateUrl: './catalog-page.component.html',
-  styleUrls: ['./catalog-page.component.sass']
+  styleUrls: ['./catalog-page.component.sass'],
+  providers: [
+    CatalogPageContextService
+  ]
 })
 export class CatalogPageComponent implements OnInit {
 
-  constructor() { }
+  // state
+  products$ = this.context.products$;
 
+  // new
+  constructor(
+    private context: CatalogPageContextService
+  ) { }
+
+  // init
   ngOnInit(): void {
-  }
 
+    // init context
+    this.context.init();
+  }
 }
---
+++ src/app/services/context/catalog-page-context.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { ProductModel } from '../../models/product-model';
+import { ApiService } from '../api.service';
+
+@Injectable()
+export class CatalogPageContextService {
+
+  // state
+  private _productsStore$ = new BehaviorSubject<ProductModel[]>([]);
+  products$ = this._productsStore$.asObservable();
+
+  // new
+  constructor(
+    private api: ApiService
+  ) {
+  }
+
+  // init
+  init() {
+    this.api.getProducts().subscribe(x => this._productsStore$.next(x));
+  }
+}
