caa8fa8e67005eef6420b78031ff57e702946886
Michael Roma <mike@romacode.com>
2022-08-17 09:12:17 -0400

    cart size in top nav

--- src/app/components/shared/top-nav/top-nav.component.ts
+++ src/app/components/shared/top-nav/top-nav.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { UserModel } from '../../../models/user-model';
 import { AuthContextService } from '../../../services/context/auth-context.service';
+import { CartContextService } from '../../../services/context/cart-context.service';
 
 @Component({
   selector: 'app-top-nav',
@@ -12,10 +13,12 @@ export class TopNavComponent implements OnInit {
 
   // state
   user$: Observable<UserModel | undefined> = this.authContext.user$
+  cartSize$ = this.cartContext.cart$.pipe(map(x => x.length));
 
   // new
   constructor(
-    private authContext: AuthContextService
+    private authContext: AuthContextService,
+    private cartContext: CartContextService
   ) { }
 
   ngOnInit(): void {
